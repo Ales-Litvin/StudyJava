3bc6a363a72fa6fea5cfcdf82907e2acc74b7552
Ales <ssaannyyaa25@gmail.com>
2020-06-19 13:35:59 +0300

    Started big task

---
+++ src/javarush/task/task28/task2811/Solution.java
@@ -0,0 +1,12 @@
+package javarush.task.task28.task2811;
+
+import java.util.LinkedHashMap;
+
+/*
+ReentrantReadWriteLock
+*/
+public class Solution {
+    public static void main(String[] args) {
+        ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
+    }
+}
