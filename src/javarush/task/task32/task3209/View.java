b3cf628ff1a293cd5652916de045ebe983e0fcae
Ales <ssaannyyaa25@gmail.com>
2020-06-22 12:09:00 +0300

    HTML Editor (7)

--- src/javarush/task/task32/task3209/View.java
+++ src/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package javarush.task.task32.task3209;
 
 import javarush.task.task32.task3209.listeners.FrameListener;
+import javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -34,7 +36,17 @@ public class View extends JFrame implements ActionListener {
 
     public void initMenuBar() { }
 
-    public void initEditor() { }
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPaneHtml = new JScrollPane(htmlTextPane);
+        Component html = tabbedPane.add("HTML", jScrollPaneHtml);
+        JScrollPane jScrollPaneText = new JScrollPane(plainTextPane);
+        Component text = tabbedPane.add("Текст", jScrollPaneText);
+        tabbedPane.setPreferredSize(new Dimension());
+        TabbedPaneChangeListener listener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(listener);
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
 
     public void initGui() {
         initMenuBar();
