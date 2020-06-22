37283c148ef6176c754268b1fc20d954bc039efc
Ales <ssaannyyaa25@gmail.com>
2020-06-22 16:00:25 +0300

    HTML Editor (13)

--- src/javarush/task/task32/task3209/View.java
+++ src/javarush/task/task32/task3209/View.java
@@ -2,8 +2,12 @@ package javarush.task.task32.task3209;
 
 import javarush.task.task32.task3209.listeners.FrameListener;
 import javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,6 +17,8 @@ public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
 
     public View(){
         try {
@@ -35,6 +41,8 @@ public class View extends JFrame implements ActionListener {
     public void setController(Controller controller) {
         this.controller = controller;
     }
+
+    public UndoListener getUndoListener() { return undoListener; }
 
     @Override
     public void actionPerformed(ActionEvent e) { }
@@ -79,4 +87,22 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void selectedTabChanged() { }
+
+    public boolean canUndo() { return undoManager.canUndo(); }
+
+    public boolean canRedo() { return undoManager.canRedo(); }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (CannotUndoException e) { ExceptionHandler.log(e); }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (CannotRedoException e) { ExceptionHandler.log(e); }
+    }
+
+    public void resetUndo() { undoManager.discardAllEdits(); }
 }
