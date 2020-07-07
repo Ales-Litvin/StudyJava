cea45d202489c367c6f42ab2ede985c56b2d6dcf
Ales <ssaannyyaa25@gmail.com>
2020-07-07 19:03:21 +0300

    Restaurant (20)  Easy

--- src/javarush/task/task27/task2712/Restaurant.java
+++ src/javarush/task/task27/task2712/Restaurant.java
@@ -2,17 +2,40 @@ package javarush.task.task27.task2712;
 
 import javarush.task.task27.task2712.kitchen.Cook;
 import javarush.task.task27.task2712.kitchen.Waiter;
+import javarush.task.task27.task2712.statistic.StatisticManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
     private final static int ORDER_CREATING_INTERVAL = 100;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Waiter waiter = new Waiter();
-        Cook cook = new Cook("Amigo");
-        cook.addObserver(waiter);
-        Tablet tablet = new Tablet(5);
-        tablet.addObserver(cook);
-        tablet.createOrder();
+        Cook cookOne = new Cook("Amigo");
+        Cook cookFirst = new Cook("Domino");
+
+        cookOne.addObserver(waiter);
+        cookFirst.addObserver(waiter);
+
+        StatisticManager statisticManager = StatisticManager.getInstance();
+        statisticManager.register(cookOne);
+        statisticManager.register(cookFirst);
+
+        List<Tablet> tablets = new ArrayList<>();
+        for (int i = 0; i < 5; i++){
+            Tablet tablet = new Tablet(i);
+            tablet.addObserver(cookFirst);
+            tablet.addObserver(cookOne);
+            tablets.add(tablet);
+        }
+
+        Thread thread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATING_INTERVAL));
+        thread.start();
+
+        Thread.sleep(1000);
+        thread.interrupt();
+
         DirectorTablet director = new DirectorTablet();
         director.printActiveVideoSet();
         director.printAdvertisementProfit();
