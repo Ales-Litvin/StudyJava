660af656b9a8e71b97f0c975dc6e4d965763e679
Ales <ssaannyyaa25@gmail.com>
2020-07-16 22:19:25 +0300

    2048 (10)

--- src/javarush/task/task35/task3513/Controller.java
+++ src/javarush/task/task35/task3513/Controller.java
@@ -1,12 +1,48 @@
 package javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+    private final static int WINNING_TILE = 2048;
+
+    public Controller(Model model) {
+        this.model = model;
+        this.view = new View(this);
+    }
 
     public Tile[][] getGameTiles(){ return model.getGameTiles(); }
 
     public int getScore(){ return model.score; }
+
+    public void resetGame(){
+        model.score = 0;
+        model.maxTile = 2;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.VK_ESCAPE) resetGame();
+
+        if (!model.canMove()) { view.isGameLost = true; }
+        if (!view.isGameLost && !view.isGameWon){
+            switch (event.getKeyCode()){
+                case KeyEvent.VK_LEFT: model.left();
+                    break;
+                case KeyEvent.VK_RIGHT: model.right();
+                    break;
+                case KeyEvent.VK_UP: model.up();
+                    break;
+                case KeyEvent.VK_DOWN: model.down();
+                    break;
+            }
+        }
+        if (model.maxTile == WINNING_TILE) view.isGameWon = true;
+        view.repaint();
+    }
 }
