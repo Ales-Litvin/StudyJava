e8e56544c60fb6615f195964347afb800e9cd15a
Ales <ssaannyyaa25@gmail.com>
2020-07-16 19:00:32 +0300

    2048 (7)

--- src/javarush/task/task35/task3513/Model.java
+++ src/javarush/task/task35/task3513/Model.java
@@ -7,7 +7,11 @@ public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
 
-    public Model(){ resetGameTiles(); }
+    public Model(){
+        resetGameTiles();
+        score = 0;
+        maxTile = 2;
+    }
 
     void resetGameTiles(){
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
@@ -22,7 +26,7 @@ public class Model {
 
     private void addTile(){
         List<Tile> emptyTiles = getEmptyTiles();
-        if (emptyTiles.size() > 0 && !emptyTiles.isEmpty()) {
+        if (emptyTiles.size() > 0) {
             Tile randomTile = emptyTiles.get((int) (emptyTiles.size() * Math.random()));
             randomTile.value = (Math.random() < 0.9 ? 2 : 4);
         }
@@ -37,5 +41,51 @@ public class Model {
             }
         }
         return emptyTiles;
+    }
+
+    int score;
+    int maxTile;
+
+    private boolean compressTiles(Tile[] tiles){
+        boolean isCompressed = false;
+        for (int i = 0; i < tiles.length; i++) {
+            if (tiles[i].isEmpty()) {
+                int j = i;
+                while (tiles[j].isEmpty() && j < tiles.length - 1){
+                    j++;
+                }
+                if (!tiles[j].isEmpty()) isCompressed = true;
+                tiles[i] = tiles[j];
+                tiles[j] = new Tile();
+            }
+        }
+        return isCompressed;
+    }
+
+    boolean mergeTiles(Tile[] tiles){
+        boolean isCompressed = false;
+        for (int i = 0; i < tiles.length - 1; i++){
+            if (tiles[i].value == tiles[i + 1].value && !tiles[i].isEmpty()){
+                isCompressed = true;
+                tiles[i].value *= 2;
+                score += tiles[i].value;
+                if (maxTile < tiles[i].value){
+                    maxTile = tiles[i].value;
+                }
+                tiles[i + 1] = new Tile();
+                compressTiles(tiles);
+            }
+        }
+        return isCompressed;
+    }
+
+    public void left(){
+        boolean wasMoved = false;
+        for (int i = 0; i < FIELD_WIDTH; i++){
+            if (compressTiles(gameTiles[i]) | mergeTiles(gameTiles[i])){
+                wasMoved = true;
+            }
+        }
+        if (wasMoved) addTile();
     }
 }
