029ce0c6f4284269cb942c7f94c967b79f722ea1
Ales <ssaannyyaa25@gmail.com>
2020-10-15 00:24:52 +0300

    Classes 15-10-2002

---
+++ src/by_epam/introduction_to_java/programming_with_classes_4/aggregation_and_composition/task02/Solution.java
@@ -0,0 +1,11 @@
+package by_epam.introduction_to_java.programming_with_classes_4.aggregation_and_composition.task02;
+
+/*
+ * Условие задачи см. class
+ * by_epam.introduction_to_java.programming_with_classes_4.aggregation_and_composition.task01.Car;
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
