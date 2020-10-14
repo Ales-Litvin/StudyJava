64a87e09f82896c52771f9f38cc6199d77a0840a
Ales <ssaannyyaa25@gmail.com>
2020-10-15 00:24:52 +0300

    Classes 15-10-2002

---
+++ src/by_epam/introduction_to_java/programming_with_classes_4/aggregation_and_composition/task02/Car.java
@@ -0,0 +1,13 @@
+package by_epam.introduction_to_java.programming_with_classes_4.aggregation_and_composition.task02;
+
+/*
+ * Условие задачи:
+ * 2. Создать объект класса Автомобиль, используя классы Колесо, Двигатель.
+ * Методы: ехать, заправляться, менять колесо, вывести на консоль марку автомобиля.
+ */
+public class Car {
+    private String model;
+
+
+
+}
