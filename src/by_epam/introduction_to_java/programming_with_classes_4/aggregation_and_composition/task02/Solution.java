8351eb4f3136d3bb00eaca1fb5f46e40bcfe0f47
Ales <ssaannyyaa25@gmail.com>
2020-10-15 21:48:48 +0300

    Programming with classes. 15-10-2020

--- src/by_epam/introduction_to_java/programming_with_classes_4/aggregation_and_composition/task02/Solution.java
+++ src/by_epam/introduction_to_java/programming_with_classes_4/aggregation_and_composition/task02/Solution.java
@@ -2,10 +2,24 @@ package by_epam.introduction_to_java.programming_with_classes_4.aggregation_and_
 
 /*
  * Условие задачи см. class
- * by_epam.introduction_to_java.programming_with_classes_4.aggregation_and_composition.task01.Car;
+ * by_epam.introduction_to_java.programming_with_classes_4.aggregation_and_composition.task01
+ * .Car;
  */
 public class Solution {
     public static void main(String[] args) {
+        // создаем объект автомобиль
+        Car car = new Car();
 
+        // вызываем метод ехать
+        car.go();
+
+        // вызвваем метод заправляться
+        car.refuel();
+
+        // менять колесо
+        car.changeWheel(new Wheel());
+
+        // выводим на консоль марку автомобиля
+        car.modelToString();
     }
 }
