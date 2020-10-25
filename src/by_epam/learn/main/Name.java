8290b47fcfc2cdd5776c8b468fdb8992d6d6ec39
Ales <ssaannyyaa25@gmail.com>
2020-10-25 18:50:53 +0300

    Start the biggest task. Library. 25-10-2020

--- src/by_epam/learn/main/Name.java
+++ src/by_epam/learn/main/Name.java
@@ -1,4 +1,11 @@
 package by_epam.learn.main;
 
-final class Name {
+import java.io.Serializable;
+
+final class Name implements Serializable {
+    public static void main(String[] args) {
+        Class clazz;
+
+    }
+
 }
