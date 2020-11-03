2f57937c1edeeb89afa76524b8544d7680e0f007
Ales <ssaannyyaa25@gmail.com>
2020-11-03 22:01:44 +0300

    Strange commit 2

--- src/by_epam/introduction_to_java/tasks_6/task03/server/entity/user/UserRole.java
+++ src/by_epam/introduction_to_java/tasks_6/task03/server/entity/user/UserRole.java
@@ -1,7 +1,6 @@
 package by_epam.introduction_to_java.tasks_6.task03.server.entity.user;
 
 import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlType;
 
 @XmlEnum
 public enum UserRole {
