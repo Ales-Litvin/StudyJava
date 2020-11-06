e3abe5573b8a8ddd032128d226e05a1f4bcd0eaa
Ales <ssaannyyaa25@gmail.com>
2020-11-06 18:33:20 +0300

    no message

--- src/by_epam/introduction_to_java/tasks_6/task04/Main.java
+++ src/by_epam/introduction_to_java/tasks_6/task04/Main.java
@@ -3,7 +3,28 @@ package by_epam.introduction_to_java.tasks_6.task04;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/*
+ * Попробуйте решить данную задачу хотя бы на 50%.
+ * Задание 4. Многопоточность. Порт . Корабли заходят в порт для
+ * разгрузки/загрузки контейнеров. Число контейнеров, находящихся в текущий момент
+ * в порту и на корабле, должно быть неотрицательным и превышающим заданную
+ * грузоподъемность судна и вместимость порта. В порту работает несколько причалов.
+ * У одного причала может стоять один корабль. Корабль может загружаться у причала
+ * или разгружаться.
+ */
 public class Main {
+    /*
+     * May be next situations:
+     * 1. In the tunnel all ships arrived for unloading, then it will work infinity, because
+     *    the port will crowded.
+     *
+     * 2. In the tunnel all ships arrived for loading, then it will work infinity, because
+     *    in the port not 'containers'.
+     *
+     * 3. In the port not 'containers' and doesn't any ship for unloading, then it will work infinity.
+     *
+     * 4. The port crowded and doesn't any ship for loading, then it will work infinity.
+     */
     public static void main(String[] args) {
         System.out.println("Available number of cores: " + Runtime.getRuntime().availableProcessors());
 
@@ -12,6 +33,9 @@ public class Main {
         ShipGenerator shipGenerator = new ShipGenerator(tunnel, 15);
 
         Port port = new Port(tunnel);
+
+        PrintInfo info = new PrintInfo(port, tunnel);
+        info.start();
 
         ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
