8faa196ac710e25edd4359b68d52dc19f435a5c3
Ales <ssaannyyaa25@gmail.com>
2020-11-10 21:11:58 +0300

    no message

--- src/by_epam/introduction_to_java/аlgorithmization_2/docomposition_using_methods/task09/AreaOfQuadrangle.java
+++
@@ -1,8 +0,0 @@
-package by_epam.introduction_to_java.аlgorithmization_2.docomposition_using_methods.task09;
-
-public class AreaOfQuadrangle {
-
-    public static int areaOfQuadrangle(int x, int y, int z, int t){
-        return ((y + z) / 2) * x;
-    }
-}
