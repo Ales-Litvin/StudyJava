d368f734b68b8c1e177670fa325bbb822dfd73e9
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-15 17:45:55 +0300

    task 2810 Aggregator(3)

--- src/javarush/task/task28/task2810/model/Provider.java
+++ src/javarush/task/task28/task2810/model/Provider.java
@@ -1,9 +1,17 @@
 package javarush.task.task28.task2810.model;
 
+import javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
 public class Provider {
     private Strategy strategy;
 
     public Provider(Strategy strategy) { this.strategy = strategy; }
 
     public void setStrategy(Strategy strategy) { this.strategy = strategy; }
+
+    public List<Vacancy> getJavaVacancies(String searchString){
+        return null;
+    }
 }
