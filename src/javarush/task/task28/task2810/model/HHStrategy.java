b5632b726294547fb3f8033e5cd068ed238d9db4
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-15 18:28:04 +0300

    task2810 Aggregator(4)

--- src/javarush/task/task28/task2810/model/HHStrategy.java
+++ src/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,4 +1,12 @@
 package javarush.task.task28.task2810.model;
 
 public class HHStrategy implements Strategy {
+
+    private static final String URL_FORMAT
+            = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+
+    // http://hh.ru/search/vacancy?text=java+%s&page=%d -- это работает
+    // http://hh.ua/search/vacancy?text=java+%s&page=%d
+
+
 }
