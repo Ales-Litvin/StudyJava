498974316cec03b899dab3b3a11d3ed0c473840e
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-15 17:34:16 +0300

    task2810 Aggregator(2). workspace.xml was delted (git rm -r --cached .idea/workspace.xml)

--- src/javarush/task/task28/task2810/Aggregator.java
+++ src/javarush/task/task28/task2810/Aggregator.java
@@ -1,7 +1,12 @@
 package javarush.task.task28.task2810;
 
+import javarush.task.task28.task2810.model.Provider;
+
 public class Aggregator {
     public static void main(String[] args) {
-        
+        Provider provider = new Provider(null);
+
+        Controller controller = new Controller(provider);
+        System.out.println(controller.toString());
     }
 }
