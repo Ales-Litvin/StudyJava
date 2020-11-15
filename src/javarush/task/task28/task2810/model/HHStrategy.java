4d8846172373e0ac62b90923179e19a516ee56d8
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-15 20:50:53 +0300

    task2810 Aggregator(6). There was strange event: HHStrategy line-25, Url don't have city and page number.

--- src/javarush/task/task28/task2810/model/HHStrategy.java
+++ src/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,11 +1,16 @@
 package javarush.task.task28.task2810.model;
 
 import javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
+    private static final String USER_AGENT = "Mozilla/5.0 (jsoup)";
+    private static final int timeout = 5 * 1000;
 
     private static final String URL_FORMAT
             = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
@@ -14,8 +19,14 @@ public class HHStrategy implements Strategy {
     // http://hh.ua/search/vacancy?text=java+%s&page=%d
 
 
-    @Override
-    public List<Vacancy> getVacancies(String searchString) {
-        return new ArrayList<>();
+    @Override public List<Vacancy> getVacancies(String searchString) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("http://hh.ua/search/vacancy?text=java+%s&page=%d").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Collections.EMPTY_LIST;
     }
 }
