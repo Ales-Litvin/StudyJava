b497c812fa4ca58262b6bdd7c2540d56d6048bab
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-16 16:37:55 +0300

    task2810 Aggregator(17) It's 'true' also for validator, and don't work in the real URL

--- src/javarush/task/task28/task2810/Aggregator.java
+++ src/javarush/task/task28/task2810/Aggregator.java
@@ -2,15 +2,18 @@ package javarush.task.task28.task2810;
 
 import javarush.task.task28.task2810.model.HHStrategy;
 import javarush.task.task28.task2810.model.Model;
+import javarush.task.task28.task2810.model.MoikrugStrategy;
 import javarush.task.task28.task2810.model.Provider;
 import javarush.task.task28.task2810.view.HtmlView;
 
 public class Aggregator {
     public static void main(String[] args) {
-        Provider provider = new Provider(new HHStrategy());
+        Provider HHProvider = new Provider(new HHStrategy());
+        Provider MKProvider = new Provider(new MoikrugStrategy());
+
 
         HtmlView view = new HtmlView();
-        Model model = new Model(view, provider);
+        Model model = new Model(view, HHProvider, MKProvider);
         Controller controller = new Controller(model);
         view.setController(controller);
         view.userCitySelectEmulationMethod();
--- src/javarush/task/task28/task2810/model/MoikrugStrategy.java
+++ src/javarush/task/task28/task2810/model/MoikrugStrategy.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HHStrategy implements Strategy {
-    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+public class MoikrugStrategy implements Strategy{
+    private static final String URL_FORMAT = "https://moikrug.ru/vacancies?q=java+%s&page=%d";
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
-    private static final String REFERRER = "strict-origin-when-cross-origin"; //"no-referrer-when-downgrade"
+    private static final String REFERRER = "https://career.habr.com/"; //"no-referrer-when-downgrade"
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
@@ -24,18 +24,19 @@ public class HHStrategy implements Strategy {
             for (int page = 0; (doc = getDocument(searchString, page)) != null; page++){
                 //Elements elements = doc.select("[data-qa=vacancy-serp__vacancy]");
                 //Elements elements = doc.select("[data-qa=\"vacancy-serp__vacancy\"]");
-                Elements elements = doc.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy");
+                Elements elements = doc.getElementsByAttributeValue("class", "job");
+                elements.addAll(doc.getElementsByAttributeValue("class", "job marked"));
 
                 if (elements.size() == 0) { break; }
 
                 for (Element element : elements) {
                     Vacancy vacancy = new Vacancy();
-                    vacancy.setTitle(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title").text());
-                    vacancy.setCity(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-address").text());
-                    vacancy.setCompanyName(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer").text());
-                    vacancy.setSalary(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-compensation").text());
-                    vacancy.setSiteName("http://hh.ua/");
-                    vacancy.setUrl(element.getElementsByAttributeValueContaining("data-qa", "vacancy-serp__vacancy-title").attr("href"));
+                    vacancy.setTitle(element.getElementsByAttributeValue("class", "title").text());
+                    vacancy.setCity(element.getElementsByAttributeValue("class", "location").text());
+                    vacancy.setCompanyName(element.getElementsByAttributeValue("class", "company_name").text());
+                    vacancy.setSalary(element.getElementsByAttributeValue("class", "count").text());
+                    vacancy.setSiteName("https://moikrug.ru"); // https://career.habr.com
+                    vacancy.setUrl("https://moikrug.ru" + element.select("div[class=title]").first().getElementsByTag("a").attr("href"));
                     vacancies.add(vacancy);
                 }
             }
