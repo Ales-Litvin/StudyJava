62375333ea94c2a974191baff846c3255ec88fed
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-16 12:44:25 +0300

    task2810 Aggragator(11)

--- src/javarush/task/task28/task2810/Controller.java
+++ src/javarush/task/task28/task2810/Controller.java
@@ -1,40 +1,17 @@
 package javarush.task.task28.task2810;
 
-import javarush.task.task28.task2810.model.Provider;
-import javarush.task.task28.task2810.vo.Vacancy;
+import javarush.task.task28.task2810.model.Model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Controller {
+    private Model model;
 
-    private Provider[] providers;
-
-    public Controller(Provider... providers) {
-        if (providers == null || providers.length == 0){
-            throw new IllegalArgumentException();
-        }
-        this.providers = providers;
+    public Controller(Model model) {
+        if (model == null) throw new IllegalArgumentException();
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Controller{");
-        sb.append("providers=").append(Arrays.toString(providers));
-        sb.append('}');
-        return sb.toString();
-    }
-
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-        try {
-            for (Provider provider : providers) {
-                vacancies.addAll(provider.getJavaVacancies("Kiev"));
-            }
-            System.out.println(vacancies.size());
-        } catch (NullPointerException e){
-            System.out.println("NPE");
-        }
+    public void onCitySelect(String cityName){
+        model.selectCity(cityName);
     }
 }
