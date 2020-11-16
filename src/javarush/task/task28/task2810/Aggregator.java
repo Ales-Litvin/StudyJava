39c3a0d5ffb554e9a8d0e65a2a350b327ffdf7bb
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-16 12:52:10 +0300

    task2810 Aggregator(12)

--- src/javarush/task/task28/task2810/Aggregator.java
+++ src/javarush/task/task28/task2810/Aggregator.java
@@ -1,11 +1,18 @@
 package javarush.task.task28.task2810;
 
 import javarush.task.task28.task2810.model.HHStrategy;
+import javarush.task.task28.task2810.model.Model;
 import javarush.task.task28.task2810.model.Provider;
+import javarush.task.task28.task2810.view.HtmlView;
 
 public class Aggregator {
     public static void main(String[] args) {
         Provider provider = new Provider(new HHStrategy());
 
+        HtmlView view = new HtmlView();
+        Model model = new Model(view, provider);
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        view.userCitySelectEmulationMethod();
     }
 }
