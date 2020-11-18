e282a138a331abb150f3539cea74ee69a5afc5e4
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-18 19:40:38 +0300

    Finished four dificult tasks

--- src/javarush/task/task39/task3908/TestPalindrome.java
+++ src/javarush/task/task39/task3908/TestPalindrome.java
@@ -2,7 +2,12 @@ package javarush.task.task39.task3908;
 
 import org.junit.Assert;
 import org.junit.Test;
-import net.bytebuddy.utility.RandomString;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class TestPalindrome {
     public static final int TESTS = 10;
@@ -16,28 +21,76 @@ public class TestPalindrome {
 
     };
 
-    public static boolean oneTest(){
-        String random = new RandomString((int) (Math.random() * 20)).nextString();
-        return Solution.isPalindromePermutationTwo(random);
-    }
-
-    public void testRandom(){
-        for (int i = 0; i < TESTS; i++){
-            Assert.assertFalse(oneTest());
-        }
-    }
-
     @Test
     public void test(){
-       for (String s : MAY_BE_PALINDROME){
-           Assert.assertTrue(Solution.isPalindromePermutationTwo(s));
-       }
-    }
-
-    @Test
-    public void test2(){
         for (String s : MAY_BE_PALINDROME){
             Assert.assertTrue(Solution.isPalindromePermutation(s));
         }
     }
+
+    @Test
+    public void testRandom(){
+        for (int i = 0; i < TESTS; i++){
+            String palindrome = getPalindrome();
+            System.out.printf("Palindrome<%s> ",palindrome);
+            palindrome = shelfString(palindrome);
+            System.out.printf("-> <%s>", palindrome);
+            boolean result = Solution.isPalindromePermutation(palindrome);
+            System.out.printf(" '%s'\n", result);
+            Assert.assertTrue(result);
+        }
+    }
+
+    @Test
+    public void testRandomThree(){
+        for (int i = 0; i < TESTS; i++){
+            String palindrome = getPalindrome();
+            System.out.printf("Palindrome<%s> ",palindrome);
+            palindrome = shelfString(palindrome);
+            System.out.printf("-> <%s>", palindrome);
+            boolean result = Solution.isPalindromePermutationThree(palindrome);
+            System.out.printf(" '%s'\n", result);
+            Assert.assertTrue(result);
+        }
+    }
+
+    public static String getRandomPalindrome(){
+        return null;
+    }
+
+    public static String generateRandomString(){
+        return new BigInteger(130, new SecureRandom()).toString(36);
+    }
+
+    public static String getPalindrome(){
+        String s1 = generateRandomString();
+        String s2 = new StringBuilder(s1).reverse().toString();
+
+        StringBuilder palindrome = new StringBuilder(s1);
+
+        if (Math.random() * 1 == 1){
+            palindrome.append(new Character((char) (Math.random() * 256)));
+        }
+        palindrome.append(s2);
+        return palindrome.toString();
+    }
+
+    public static String shelfString(String string){
+        List<Character> chars = new ArrayList<>(string.length());
+
+        for (char c : string.toCharArray()){
+            chars.add(c);
+        }
+
+        Collections.shuffle(chars);
+
+        char[] copy = new char[chars.size()];
+
+        for (int i = 0; i < copy.length; i++) {
+            copy[i] = chars.get(i);
+        }
+
+        return String.valueOf(copy);
+    }
+
 }
--- src/javarush/task/task39/task3909/Solution.java
+++ src/javarush/task/task39/task3909/Solution.java
@@ -13,58 +13,55 @@ package javarush.task.task39.task3909;
 4. Метод isOneEditAway должен быть публичным.
 */
 
-public class Solution {
+public class
+Solution {
     public static void main(String[] args) {
-        System.out.println(isOneEditAway(
-                "vna  ;dljd  lksjdf",
-                "vna;dljd  lksjdf"
-        ) + " : false");
-
+        System.out.println(isOneEditAway("vna  ;dljd", "vna;dljd") + " : false");
+        System.out.println(isOneEditAway("ботинок", "почтичто") + " : false");
         System.out.println(isOneEditAway("dddddd", "dd  dddd") + " : false");
         System.out.println(isOneEditAway("123", "1023")  + " : true");
         System.out.println(isOneEditAway("01", "12")  + " : true");
         System.out.println(isOneEditAway("123", "1453")  + " : true");
         System.out.println(isOneEditAway("01", "102")  + " : false");
         System.out.println(isOneEditAway("dddddd", " dddddd")  + " : true");
-        System.out.println(isOneEditAway("123", "1453") + " : true");
+        System.out.println(isOneEditAway("123", "1423") + " : true");
         System.out.println(isOneEditAway("2", "33") + " : false");
 
     }
 
-    // Это все не работает
     // разделить на 2 случая строки одинаковой длинны и строки разной длинны
-    //
-    public static boolean isOneEditAway(String first, String second){
-        char[] charsOne = first.toCharArray();
-        char[] charsTwo = second.toCharArray();
+    public static boolean isOneEditAway(String first, String second) {
+        int len1 = first.length();
+        int len2 = second.length();
+        int delta = Math.abs(len1 - len2);
 
-        int countChanges = 0;
+        if (delta > 1) return false;
 
-        int i = 0, j = 0;
+        if (first.equals("") && second.equals("")) return true;
 
-        while (i < charsOne.length && j < charsTwo.length) {
-            if (charsOne[i] == charsTwo[j]){
-                i++; j++;
-            } else if (charsOne[i] != charsTwo[j]){
+        if(first.equals(second)) return true;
 
-                if (charsOne.length > i + 1 && charsOne[i + 1] == charsTwo[j]) {
-                    i++;
-                } else if (charsTwo.length > j + 1 && charsOne[i] == charsTwo[j + 1]){
-                    j++;
-                } else if (i != 0 && charsOne[i - 1] == charsTwo[j]){
-                    i++;
-                } else if (j != 0 && charsOne[i] == charsTwo[j - 1]){
-                    j++;
+        StringBuffer s1 = (first.length() >= second.length()) ? new StringBuffer(first) : new StringBuffer(second);
+        StringBuffer s2 = (first.length() < second.length()) ? new StringBuffer(first) : new StringBuffer(second);
+
+        // s2.length < s1.length
+        for (int i = 0; i < s2.length(); i++) {
+
+            if (s1.charAt(i) != s2.charAt(i)) {
+
+                if (delta != 0) {
+                    s1.deleteCharAt(i);
+                } else {
+                    s1.deleteCharAt(i);
+                    s2.deleteCharAt(i);
                 }
 
-                countChanges++;
-                i++; j++;
+                break;
             }
         }
 
+        if (s1.length() != s2.length()) s1.deleteCharAt(s1.length() - 1);
 
-        if (i != j) return false;
-
-        return countChanges == 1 || countChanges == 0;
+        return s1.toString().equals(s2.toString());
     }
 }
