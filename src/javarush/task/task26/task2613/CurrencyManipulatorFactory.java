cae6df2ed60752e2d6203ae1e6617c4a1fe0a7c5
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-19 16:19:27 +0300

    task2613 CashMachine(3)

---
+++ src/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
@@ -0,0 +1,15 @@
+package javarush.task.task26.task2613;
+
+import java.util.Map;
+
+public class CurrencyManipulatorFactory {
+
+    private CurrencyManipulatorFactory() { }
+
+    private static Map<String, CurrencyManipulator> map;
+
+    public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode){
+
+        return null;
+    }
+}
