2c4c99fe82616c756f343527a1f094cb107fe344
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-20 17:09:31 +0300

    task2613 CashMachine(13) May be 'true'

--- src/javarush/task/task26/task2613/command/ExitCommand.java
+++ src/javarush/task/task26/task2613/command/ExitCommand.java
@@ -1,15 +1,23 @@
 package javarush.task.task26.task2613.command;
 
+import javarush.task.task26.task2613.CashMachine;
 import javarush.task.task26.task2613.ConsoleHelper;
 import javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 class ExitCommand  implements Command {
+
+    private final ResourceBundle res = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.exit_en");
 
     @Override
     public void execute() throws InterruptOperationException {
-        ConsoleHelper.writeMessage("You real want to exit yes-'y' or no-'n'");
-        if (ConsoleHelper.readString().equalsIgnoreCase("y")){
-            ConsoleHelper.writeMessage("Bye!");
+        Locale.setDefault(Locale.ENGLISH);
+
+        ConsoleHelper.writeMessage(res.getString("exit.question.y.n"));
+        if (ConsoleHelper.readString().trim().equalsIgnoreCase("y")){
+            ConsoleHelper.writeMessage(res.getString("thank.message"));
         }
     }
 }
