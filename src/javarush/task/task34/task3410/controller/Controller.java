5162821a27c732cdfa22b2617ca9d66e5c9e21ed
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-23 13:40:17 +0300

    task3410 Sokoban (1)

---
+++ src/javarush/task/task34/task3410/controller/Controller.java
@@ -0,0 +1,20 @@
+package javarush.task.task34.task3410.controller;
+
+import javarush.task.task34.task3410.model.Model;
+import javarush.task.task34.task3410.view.View;
+
+public class Controller {
+    private View view;
+
+    private Model model;
+
+    public Controller() {
+        this.view = new View(this);
+        this.model = new Model();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/javarush/task/task34/task3410/view/View.java
@@ -0,0 +1,13 @@
+package javarush.task.task34.task3410.view;
+
+import javarush.task.task34.task3410.controller.Controller;
+
+import javax.swing.*;
+
+public class View extends JFrame {
+    private Controller controller;
+
+    public View(Controller controller){
+        this.controller = controller;
+    }
+}
