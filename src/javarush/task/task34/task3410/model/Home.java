5ac40eb8169f7b6ba1ca62012c2c8e745c237be0
Aliaksandr-Rachko <rachko.a.ch@gmail.com>
2020-11-24 13:37:13 +0300

    task3410 Sokoban(6)

---
+++ src/javarush/task/task34/task3410/model/Home.java
@@ -0,0 +1,16 @@
+package javarush.task.task34.task3410.model;
+
+import java.awt.*;
+
+public class Home extends GameObject{
+
+    public Home(int x, int y) {
+        super(x, y,2,2);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.RED);
+        graphics.drawOval(getX() - getHeight() / 2, getY() - getWidth(), getWidth(), getHeight());
+    }
+}
